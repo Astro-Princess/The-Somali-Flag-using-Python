540d964dbfbc9ca38d17f5c9c91ae64e1ec8bc6d
Astro-Princess <101041528+Astro-Princess@users.noreply.github.com>
2022-03-13 11:44:17 +0600

    Add files via upload

---
+++ The Somali Flag.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.speed(2)
+turtle.color("white")
+turtle.bgcolor("#478eff")
+turtle.fillcolor("white")
+
+turtle.begin_fill()
+
+for i in range(5):
+    turtle.forward(100)
+    turtle.right(144)
+
+turtle.end_fill()
